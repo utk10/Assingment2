de1dadf03a05f4b97c239a8c52e3d7de60bf55cb
utkarsh <stiwarik17@gmail.com>
2024-01-24 16:20:17 +0530

    New Commits

--- JavaCodes/first.java
+++ JavaCodes/first.java
@@ -2,5 +2,6 @@ import java.util.*;
 class first{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("This is my first program in Java");
     }
 }
